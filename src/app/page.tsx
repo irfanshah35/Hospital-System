1b8b27df6bde68b63641503ebb180bafb1079fdf
Aqeel Anwar <aqeelanwar677@gmail.com>
2025-10-16 17:02:17 +0500

    feat: add header component and integrate US flag SVG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Header from "@/components/header";
 import SideBar from "@/components/sidebar";
 
 export default function Home() {
   return (
-    <SideBar/>
+    <>
+      <Header />
+      <SideBar />
+    </>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -62,8 +62,19 @@ export default function Header() {
         >
             <div className="flex items-center w-full max-w-[1400px] mx-auto py-[4px]"
             >
-                <div className="p-[8px] !pr-0">
-                    <div className='flex items-center gap-2 px-[25px] mr-4 w-[244px] justify-center'>
+                <div className="p-[8px] !pr-0 flex items-center">
+                    <button className="text-gray-700 hover:text-gray-900 w-[48px] h-[48px] flex justify-center items-center lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-6 h-6 text-gray-700 relative left-[2px] top-[1px]"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                        >
+                            <path d="M3 6h18v2H3V6zm0 5h18v2H3v-2zm0 5h18v2H3v-2z" />
+                        </svg>
+
+                    </button>
+                    <div className='flex items-center gap-[10px] px-[25px] mr-4 w-[244px] justify-center'>
                         <img
                             src="assets/header/logo.png"
                             alt="Cliniva Logo"
@@ -88,39 +99,48 @@ export default function Header() {
 
                     </button>
 
-                    <div className="hidden md:flex items-center gap-4">
-                        <button className="relative text-gray-600 hover:text-gray-900">
+                    <div className="flex items-center">
+                        <button className="relative text-gray-600 hover:text-gray-900 flex justify-center items-center w-[48px] h-[48px]">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
-                                className="w-6 h-6 text-gray-700"
+                                className='w-[24px] h-6'
                             >
-                                <path d="M5 14H5v5h5v-2H7v-3zm0-4h2V7h3V5H5v5zm10 7h-3v2h5v-5h-2v3zm-3-9V5h5v5h-2V7h-3z" />
+                                <path d="M4 4h6v2H6v4H4V4zm14 0v6h-2V6h-4V4h6zm-6 16h6v-6h-2v4h-4v2zM4 20h6v-2H6v-4H4v6z" />
                             </svg>
                         </button>
-                        <button
-                            className="relative text-gray-600 hover:text-gray-900"
-                            aria-label="Notifications"
-                        >
-                            <i className="material-icons-outlined text-[22px]">notifications</i>
+                        <button className="relative text-gray-600 hover:text-gray-900 flex justify-center items-center w-[48px] h-[48px]">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            >
+                                <path d="M18 8a6 6 0 0 0-12 0c0 7-3 9-3 9h18s-3-2-3-9" />
+                                <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+                            </svg>
+
                             <span
-                                className="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] font-semibold rounded-full px-[5px] py-[1px]"
+                                className="absolute top-1 right-[12px] bg-red-500 text-white text-[10px] font-semibold rounded-full px-[5px] py-[1px]"
                             >
                                 3
                             </span>
                         </button>
                         <button
-                            className="flex items-center gap-2 text-gray-700 hover:text-gray-900"
+                            className="gap-2 text-gray-700 hover:text-gray-900 flex justify-center items-center w-[48px] h-[48px]"
                             aria-label="Language"
                         >
                             <img
-                                src="assets/images/flags/us.svg"
+                                src="assets/header/us.svg"
                                 alt="English"
                                 className="h-4 w-auto"
                             />
                         </button>
-                        <div className="flex items-center gap-2">
+                        <div className="w-[131px] h-[48px] flex justify-center items-center gap-2">
                             <span className="text-sm font-medium text-gray-800">Ella Jones</span>
                             <img
                                 src="assets/images/user/admin.jpg"
