a53e642d73fee082c208ebe24ae3a2ead373cf40
Aqeel Anwar <aqeelanwar677@gmail.com>
2025-10-20 17:38:15 +0500

    fix(layout): adjust margin for layout component and update SVG attributes in ViewAppointments

--- src/components/appointments/view-appointments/index.tsx
+++ src/components/appointments/view-appointments/index.tsx
@@ -123,7 +123,7 @@ export default function ViewAppointments() {
                                             className="w-[212px] h-[45px] rounded-[5px] border-0 bg-white text-[14px] font-medium text-[#0000008a] px-[50px] py-2 focus:outline-none"
                                         ></input>
                                         <span className='absolute left-2 top-2'>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search-icon lucide-search w-6 h-6"><path d="m21 21-4.34-4.34" /><circle cx="11" cy="11" r="8" /></svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search-icon lucide-search w-6 h-6"><path d="m21 21-4.34-4.34" /><circle cx="11" cy="11" r="8" /></svg>
                                         </span>
                                     </label>
                                 </div>
