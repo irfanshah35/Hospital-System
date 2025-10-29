5adb5e843b8cc8820697229ceed184d5d6617672
awais <154322092+awaixrao@users.noreply.github.com>
2025-10-29 11:41:35 +0500

    records

---
+++ src/app/[auth]/records/birth-records/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import BirthRecordsComponent from '@/components/birth-records-component'
+import React from 'react'
+
+export default function BirthRecords() {
+  return (
+    <div>
+      <BirthRecordsComponent />
+    </div>
+  )
+}
---
+++ src/app/[auth]/records/death-records/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import DeathRecordsComponent from '@/components/death-records-component'
+import React from 'react'
+
+export default function DeathRecords() {
+  return (
+    <div>
+      <DeathRecordsComponent />
+    </div>
+  )
+}
