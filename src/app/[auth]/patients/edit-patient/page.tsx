2c46aba8b4fa01a491dbe71752c4586fb7907a42
Aqeel Anwar <aqeelanwar677@gmail.com>
2025-10-29 10:47:41 +0500

    feat: Add Patient Records page with CRUD functionality and XLSX export
    - Implemented PatientRecordPage component for managing patient records.
    - Added functionality to fetch, edit, delete, and download patient data in XLSX format.
    - Integrated a paginator for navigating through patient records.
    - Updated AdminSidebar paths to reflect new routing structure for patient management.

---
+++ src/app/[auth]/patients/edit-patient/page.tsx
@@ -0,0 +1,10 @@
+import EditPatientPage from '@/components/patients/edit-patient-page'
+import React from 'react'
+
+export default function EditPatient() {
+  return (
+    <div>
+        <EditPatientPage />
+    </div>
+  )
+}
---
+++ src/app/[auth]/patients/patient-profile/page.tsx
@@ -0,0 +1,10 @@
+import PatientProfilePage from '@/components/patients/patient-profile-page'
+import React from 'react'
+
+export default function PatientProfile() {
+  return (
+    <div>
+        <PatientProfilePage />
+    </div>
+  )
+}
