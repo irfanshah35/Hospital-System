910651fe50d1f3f60a309f52a73dd8498d05ef87
Aqeel Anwar <aqeelanwar677@gmail.com>
2025-11-06 16:26:56 +0500

    feat: Add tasks page and integrate task management functionality

---
+++ src/app/[auth]/tasks/page.tsx
@@ -0,0 +1,10 @@
+import TasksPage from '@/components/Apps-pages/tasks-page'
+import React from 'react'
+
+export default function Tasks() {
+  return (
+    <div>
+        <TasksPage />
+    </div>
+  )
+}
